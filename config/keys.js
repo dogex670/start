039be160c2125ec3b0eac9b978e2da68dedb4d03
Jose Carlos Bolanos Marquez <josex670@hotmail.com>
2018-06-15 12:17:55 -0700

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,8 @@
 module.exports = {
-    mongoURI:  'mongodb://guillenvd:password1@ds159110.mlab.com:59110/mongo-bootcam'
+    mongoURI: 'mongodb://josex670:dogex670@ds261450.mlab.com:61450/bootcamp2018'
+    // 'mongodb://guillenvd:password1@ds159110.mlab.com:59110/mongo-bootcam'
 }
+
+//{ user: "josex670", account: "josex670" }
+
+//mongodb://josex670:dogex670@ds261450.mlab.com:61450/bootcamp2018
--- server.js
+++ server.js
@@ -13,7 +13,11 @@ mongoose
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('hello!!!')); //router to a homepage
- 
+ // el slash solo '/' es home
+
+//Si no hace un callback
+
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=> console.log('Server running on port ', port));
+app.listen(port, ()=> console.log('Server running on port', port));
